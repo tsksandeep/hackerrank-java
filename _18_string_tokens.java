c6f3202a8871d931bcaca38146ac7c450ea3fb8f
tsksandeep <tsksandeep11@gmail.com>
2022-09-17 18:51:44 +0530

    Added 18

---
+++ _18_string_tokens.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class _18_string_tokens {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        var line = bufferedReader.readLine().trim();
+
+        var lineSplit = line.split("[!,?._'@ ]");
+
+        var count = 0;
+        List<String> newList = new ArrayList<String>();
+
+        for (String word : lineSplit) {
+            if (word.equals("")) {
+                continue;
+            }
+            count++;
+            newList.add(word);
+        }
+
+        System.out.println(count);
+
+        for (String word : newList) {
+            System.out.println(word);
+        }
+
+        bufferedReader.close();
+    }
+}
