4fdcafad69594a6e24fc070e8eada93c10ae343c
tsksandeep <tsksandeep11@gmail.com>
2022-10-04 08:57:33 +0530

    Added 43, 44, 45

---
+++ _44_factory.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+interface Food {
+    public String getType();
+}
+
+class Pizza implements Food {
+    public String getType() {
+        return "Someone ordered a Fast Food!";
+    }
+}
+
+class Cake implements Food {
+
+    public String getType() {
+        return "Someone ordered a Dessert!";
+    }
+}
+
+class FoodFactory {
+    public Food getFood(String order) {
+        if (order.equals("cake")) {
+            return new Cake();
+        }
+        return new Pizza();
+    }
+}
+
+public class _44_factory {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        FoodFactory foodFactory = new FoodFactory();
+
+        Food food = foodFactory.getFood(sc.nextLine());
+
+        System.out.println("The factory returned " + food.getClass());
+        System.out.println(food.getType());
+    }
+
+}
